9f9d11d340e161a7cc19741492a8c45bb3a8e207
LOWYONGQING <lowyq-wp22@student.tarc.edu.my>
2025-09-21 21:04:12 +0800

    LYQ

--- lib/booking_record.dart
+++ lib/booking_record.dart
@@ -6,7 +6,7 @@ class BookingRecord{
   final String description;
   final DateTime date;
   final TimeOfDay time;
-  final int serviceTypeId;
+  final String serviceTypeName;
 
   BookingRecord({
     required this.phoneNum,
@@ -14,22 +14,23 @@ class BookingRecord{
     required this.description,
     required this.date,
     required this.time,
-    required this.serviceTypeId,
+    required this.serviceTypeName,
   });
 
   factory BookingRecord.fromJson(Map<String, dynamic> json) {
-    final timeParts = (json['Time'] as String).split(':');
+    final timeString = json['Time'] as String;
+    final timeParts = timeString.split(':');
     final parsedTime = TimeOfDay(
       hour: int.parse(timeParts[0]),
       minute: int.parse(timeParts[1]),
     );
     return BookingRecord(
-      phoneNum: json['PhoneNum'] as String,
-      carPlate: json['CarPlate'] as String,
-      description: json['Description'] as String,
-      date: DateTime.parse(json['Date'] as String),
-      time: parsedTime,
-      serviceTypeId: json['ServiceTypeId'] as int,
-    );
-  }
+        phoneNum: json['PhoneNum'] as String,
+        carPlate: json['CarPlate'] as String,
+        description: json['Description'] as String,
+        date: DateTime.parse(json['Date'] as String),
+        time: parsedTime,
+        serviceTypeName: json['ServiceType'] as String,
+        );
+    }
 }
